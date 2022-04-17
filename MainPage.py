2bae1bcbfd2eca4172de6e34ee6b05ea95df9b2c
unknown <msheikh292@gmail.com>
2022-04-17 16:08:12 +0430

    add Pages & Tests

--- MainPage.py
+++
@@ -1,8 +0,0 @@
-class MainPage:
-    def __init__(self, driver):
-        self.driver = driver
-        self.dashboard_lable_id = 'menu_dashboard_index'
-
-    def check_main_page(self):
-        self.driver.find_element('id', self.dashboard_lable_id)
-
---
+++ Tests/LoginTest.py
@@ -0,0 +1,29 @@
+from Pages.Login import Login
+from Pages.MainPage import MainPage
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+import unittest
+
+
+class LoginTest(unittest.TestCase):
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        cls.driver.implicitly_wait(3)
+        cls.driver.maximize_window()
+
+    def test_valid_login(self):
+        self.driver.get('https://opensource-demo.orangehrmlive.com/')
+        login = Login(driver=self.driver)
+        main_page = MainPage(driver=self.driver)
+        login.enter_username('Admin')
+        login.enter_password('admin123')
+        login.click_on_login_button()
+        main_page.check_main_page()
+        sleep(1)
+
+
+    def tearDownClass(cls) -> None:
+        cls.driver.close()
+        cls.driver.quit()
+
