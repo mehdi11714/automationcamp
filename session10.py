c75f929c24a22de7a06670c822251c656eeb09e4
HPDS <msheikh292@gmail.com>
2022-04-25 11:51:18 +0430

    add session8

---
+++ session10.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+actions = ActionChains(driver=driver)
+driver.implicitly_wait(3)
+driver.maximize_window()
+driver.get("https://www.imdb.com/chart/top/")
+sleep(2)
+element = driver.find_element('link text', 'The Wizard of Oz')
+driver.execute_script("arguments[0].scrollIntoView();", element)
+sleep(2)
+
+def scroll_to_find_element(locator):
+    for i in range(10):
